7e97f5ffbde5b2f23a15b8718c2fbc5424920697
Ronan Burke <burkuscat@gmail.com>
2024-05-29 20:45:08 +0100

    2.22 Using Signals To Reset The Game 2.23 Parallax Background & Text Labels

--- speedy_saucer/player.gd
+++ speedy_saucer/player.gd
@@ -2,10 +2,6 @@ extends RigidBody2D
 
 var force = 800
 
-func _ready():
-	var addition_result = add(10, 5)
-	print(addition_result)
-
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force, 0))
@@ -15,10 +11,3 @@ func _physics_process(delta):
 		apply_force(Vector2(0, -force))
 	if Input.is_action_pressed("move_down"):
 		apply_force(Vector2(0, force))
-
-func test():
-	var t = 10
-	return (t+10)
-
-func add(num1, num2):
-	return num1 + num2
