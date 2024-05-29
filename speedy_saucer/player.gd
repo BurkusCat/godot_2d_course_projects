42456c14bc903d34c7f4447187addb1920054356
Ronan Burke <burkuscat@gmail.com>
2024-05-29 19:43:18 +0100

    2.16 If Statement & Comparison Operators

--- speedy_saucer/player.gd
+++ speedy_saucer/player.gd
@@ -2,8 +2,23 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#apply_impulse(Vector2(25, 0))
-	pass
+	# < > == != >= <=
+	var test = 5
+	if test > 5:
+		print("True")
+		print("Second message")
+	elif test == 5:
+		print("Test is 5")
+	else:
+		print("False")
+	
+	var message = "Greet"
+	if message == "Hello":
+		print("Hi there")
+	elif message == "Bye":
+		print("Goodbye!")
+	elif message == "Thanks":
+		print("Invalid message")
 
 func _process(delta):
 	pass
