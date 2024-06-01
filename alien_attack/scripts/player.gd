b3ccaf8ba447d469b0110b3f6c184b37c8b4eba3
Ronan Burke <burkuscat@gmail.com>
2024-06-01 20:47:50 +0100

    3.8 Creating References To Nodes

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const speed = 18000
 
 var rocket_scene = preload("res://scenes/rocket.tscn")
+@onready var rocket_container = $RocketContainer
 
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
@@ -10,7 +11,8 @@ func _process(delta):
 		
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
 
 func _physics_process(delta):
