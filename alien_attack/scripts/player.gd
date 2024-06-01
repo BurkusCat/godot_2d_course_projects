1e73dc8c64ff5e1b3aca284a65ea377c84af1506
Ronan Burke <burkuscat@gmail.com>
2024-06-01 21:56:14 +0100

    3.12 Spawn Enemy Function

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -11,9 +11,9 @@ func _process(delta):
 		
 func shoot():
 	var rocket_instance = rocket_scene.instantiate()
-	rocket_container.add_child(rocket_instance)
 	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
+	rocket_container.add_child(rocket_instance)
 
 func _physics_process(delta):
 	velocity = Vector2(0, 0)
