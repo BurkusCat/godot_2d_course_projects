469264f0ca695139f2bcb5ca80af059943a9fbaf
Ronan Burke <burkuscat@gmail.com>
2024-06-01 19:43:36 +0100

    3.6 Understanding Delta Time

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
-const speed = 300
+const speed = 18000
 
 func _physics_process(delta):
 	velocity = Vector2(0, 0)
 	
+	var move_speed = speed * delta
+	
 	if Input.is_action_pressed("move_right"):
-		velocity.x = speed
+		velocity.x = move_speed
 	if Input.is_action_pressed("move_left"):
-		velocity.x = -speed
+		velocity.x = -move_speed
 	if Input.is_action_pressed("move_down"):
-		velocity.y = speed
+		velocity.y = move_speed
 	if Input.is_action_pressed("move_up"):
-		velocity.y = -speed
+		velocity.y = -move_speed
 	
 	move_and_slide()
 	
