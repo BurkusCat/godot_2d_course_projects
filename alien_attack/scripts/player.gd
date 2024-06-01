bff6f96258afbff1473e5230c56ffdd5924f4135
Ronan Burke <burkuscat@gmail.com>
2024-06-01 19:24:29 +0100

    3.4 Clamping Players Position

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 const speed = 300
 
 func _physics_process(delta):
-	print(velocity)
 	velocity = Vector2(0, 0)
 	
 	if Input.is_action_pressed("move_right"):
@@ -16,3 +15,8 @@ func _physics_process(delta):
 		velocity.y = -speed
 	
 	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
+	print(position)
