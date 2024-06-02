e911ce77668cdba164b234a4adc37baae8b2ae7a
Ronan Burke <burkuscat@gmail.com>
2024-06-02 17:48:25 +0100

    4.4 Player Movement

--- martian_mike/scenes/player.gd
+++ martian_mike/scenes/player.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
-@onready var animated_sprite = $AnimatedSprite2D	
+@export var gravity = 400
+@export var speed = 125
+@export var jump_force = 200
 
-func _process(delta):
-	if Input.is_action_just_pressed("move_right"):
-		animated_sprite.play("run")
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		
+		# cap max fall speed
+		if velocity.y > 500:
+			velocity.y = 500
+		
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jump_force
+	
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	move_and_slide()
