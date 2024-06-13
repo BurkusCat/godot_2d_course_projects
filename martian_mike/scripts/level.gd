48009ece8cf0a49561a9a62fc0e32a9fe40535a4
Ronan Burke <burkuscat@gmail.com>
2024-06-13 20:19:49 +0100

    4.20 Level Timer

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -5,7 +5,15 @@ extends Node2D
 @onready var start = $Start
 @onready var exit = $Exit
 @onready var death_zone = $Deathzone
+
+@export var level_time = 5
+
 var player = null
+
+var timer_node = null
+var time_left
+
+var win = false
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
@@ -18,6 +26,23 @@ func _ready():
 		
 	exit.body_entered.connect(_on_exit_body_entered)
 	death_zone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+	
+func _on_level_timer_timeout():
+	if win == false:
+		time_left -= 1
+		print(time_left)
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -38,6 +63,7 @@ func reset_player():
 func _on_exit_body_entered(body):
 	if body is Player:
 		if next_level != null:
+			win = true
 			exit.animate()
 			body.active = false
 			await get_tree().create_timer(1.5).timeout
