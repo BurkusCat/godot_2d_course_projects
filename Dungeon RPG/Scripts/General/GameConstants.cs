b9bdbec60768bc36e2e2d24b1e896fb992c495c0
Ronan Burke <burkuscat@gmail.com>
2024-06-22 20:21:26 +0100

    3.8 Transitioning to the Dash State

--- Dungeon RPG/Scripts/General/GameConstants.cs
+++ Dungeon RPG/Scripts/General/GameConstants.cs
@@ -13,4 +13,5 @@ public partial class GameConstants
     public const string INPUT_MOVE_RIGHT = "MoveRight";
     public const string INPUT_MOVE_FORWARD = "MoveForward";
     public const string INPUT_MOVE_BACKWARD = "MoveBackward";
+    public const string INPUT_MOVE_DASH = "Dash";
 }
