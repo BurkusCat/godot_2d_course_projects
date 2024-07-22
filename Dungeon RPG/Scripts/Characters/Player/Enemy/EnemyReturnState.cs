593d89f3b564ddd0a005f723c40aff3169c05b5a
Ronan Burke <burkuscat@gmail.com>
2024-07-22 22:10:12 +0100

    5.5 Creating the Enemy Return State

---
+++ Dungeon RPG/Scripts/Characters/Player/Enemy/EnemyReturnState.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public partial class EnemyReturnState : EnemyState
+{
+    private Vector3 destination;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        Vector3 localPos = characterNode.PathNode.Curve
+            .GetPointPosition(0);
+        Vector3 globalPos = characterNode.PathNode.GlobalPosition;
+
+        destination = localPos + globalPos;
+    }
+
+    protected override void EnterState()
+    {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
+
+
+        characterNode.GlobalPosition = destination;
+    }
+}
