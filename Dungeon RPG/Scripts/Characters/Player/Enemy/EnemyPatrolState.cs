d549ebcdb9aca5a4070f0d508dcf77eafec47a5c
Ronan Burke <burkuscat@gmail.com>
2024-08-24 17:40:57 +0100

    5.11 Pausing on Patrols

--- Dungeon RPG/Scripts/Characters/Player/Enemy/EnemyPatrolState.cs
+++ Dungeon RPG/Scripts/Characters/Player/Enemy/EnemyPatrolState.cs
@@ -1,8 +1,11 @@
-using System;
 using Godot;
 
 public partial class EnemyPatrolState : EnemyState
 {
+    [Export] public Timer idleTimerNode { get; private set; }
+
+    [Export(PropertyHint.Range, "0,20,0.1")] public float maxIdleTime = 4;
+
     private int pointIndex = 0;
 
     protected override void EnterState()
@@ -17,15 +20,34 @@ public partial class EnemyPatrolState : EnemyState
         characterNode.AgentNode.TargetPosition = destination;
 
         characterNode.AgentNode.NavigationFinished += HandleNavigationFinished;
+        idleTimerNode.Timeout += HandleTimeout;
     }
+
 
     public override void _PhysicsProcess(double delta)
     {
+        if (!idleTimerNode.IsStopped())
+        {
+            return;
+        }
+
         Move();
     }
 
     private void HandleNavigationFinished()
     {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_IDLE);
+
+        RandomNumberGenerator rng = new();
+        idleTimerNode.WaitTime = rng.RandfRange(0f, maxIdleTime);
+
+        idleTimerNode.Start();
+    }
+
+    private void HandleTimeout()
+    {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_MOVE);
+
         pointIndex = Mathf.Wrap(
             pointIndex + 1,
             0,
