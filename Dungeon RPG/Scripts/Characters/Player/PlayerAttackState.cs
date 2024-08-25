f534fbe52db949d143d9f4e16dac4ed8c803082b
Ronan Burke <burkuscat@gmail.com>
2024-08-25 15:27:58 +0100

    6.4 Resetting the Combo Counter With a Timer

--- Dungeon RPG/Scripts/Characters/Player/PlayerAttackState.cs
+++ Dungeon RPG/Scripts/Characters/Player/PlayerAttackState.cs
@@ -2,19 +2,33 @@ using Godot;
 
 public partial class PlayerAttackState : PlayerState
 {
+    [Export] private Timer comboTimerNode;
+
     private int comboCounter = 1;
     private int maxComboCount = 2;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        comboTimerNode.Timeout += () => comboCounter = 1;
+    }
+
     protected override void EnterState()
     {
-        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter);
+        characterNode.AnimPlayerNode.Play(
+            GameConstants.ANIM_ATTACK + comboCounter,
+            customSpeed: 1.5f);
 
         characterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
+
+        comboTimerNode.Stop();
     }
 
     protected override void ExitState()
     {
         characterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
+        comboTimerNode.Start();
     }
 
     private void HandleAnimationFinished(StringName animName)
