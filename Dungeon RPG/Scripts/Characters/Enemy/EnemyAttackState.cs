fba0c4fc842166b7aab5fb5ca09efd02901891bb
Ronan Burke <burkuscat@gmail.com>
2024-08-25 17:03:50 +0100

    6.14 Finishing the Enemy Attack State

--- Dungeon RPG/Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Dungeon RPG/Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -14,6 +14,45 @@ public partial class EnemyAttackState : EnemyState
             .First();
 
         targetPosition = target.GlobalPosition;
+
+        characterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        characterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
+    }
+
+    private void HandleAnimationFinished(StringName animName)
+    {
+        characterNode.ToggleHitbox(true);
+
+        Node3D target = characterNode.AttackAreaNode
+            .GetOverlappingBodies()
+            .FirstOrDefault();
+
+        if (target == null)
+        {
+            Node3D chaseTarget = characterNode.ChaseAreaNode
+                .GetOverlappingBodies()
+                .FirstOrDefault();
+
+            if (chaseTarget == null)
+            {
+                characterNode.StateMachineNode.SwitchState<EnemyReturnState>();
+                return;
+            }
+
+            characterNode.StateMachineNode.SwitchState<EnemyChaseState>();
+            return;
+        }
+
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_ATTACK);
+        targetPosition = target.GlobalPosition;
+
+        // flip the enemy to attack towards hte player
+        var direction = characterNode.GlobalPosition.DirectionTo(targetPosition);
+        characterNode.SpriteNode.FlipH = direction.X < 0;
     }
 
     private void PerformHit()
