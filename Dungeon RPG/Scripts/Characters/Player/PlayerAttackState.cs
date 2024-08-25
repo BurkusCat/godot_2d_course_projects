e322f8dea5168b47f6b9d901ffdc264759be0768
Ronan Burke <burkuscat@gmail.com>
2024-08-25 15:07:54 +0100

    6.3 Combo Counters

--- Dungeon RPG/Scripts/Characters/Player/PlayerAttackState.cs
+++ Dungeon RPG/Scripts/Characters/Player/PlayerAttackState.cs
@@ -1,6 +1,32 @@
 using Godot;
-using System;
 
 public partial class PlayerAttackState : PlayerState
 {
+    private int comboCounter = 1;
+    private int maxComboCount = 2;
+
+    protected override void EnterState()
+    {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter);
+
+        characterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        characterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
+    }
+
+    private void HandleAnimationFinished(StringName animName)
+    {
+        comboCounter++;
+
+        // move combo count to next attack
+        comboCounter = Mathf.Wrap(
+            comboCounter,
+            1,
+            maxComboCount + 1);
+
+        characterNode.StateMachineNode.SwitchState<PlayerIdleState>();
+    }
 }
