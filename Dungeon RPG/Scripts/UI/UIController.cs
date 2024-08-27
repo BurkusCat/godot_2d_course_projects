b528807af1ed58ecfcb9eb989af6c9b38de351f3
Ronan Burke <burkuscat@gmail.com>
2024-08-27 21:06:33 +0100

    8.2 Preparing the Reward Feature

--- Dungeon RPG/Scripts/UI/UIController.cs
+++ Dungeon RPG/Scripts/UI/UIController.cs
@@ -7,6 +7,8 @@ public partial class UIController : Control
 {
     private Dictionary<ContainerType, UIContainer> containers;
 
+    private bool canPause = false;
+
     public override void _Ready()
     {
         containers = GetChildren()
@@ -19,10 +21,31 @@ public partial class UIController : Control
 
         GameEvents.OnEndGame += HandleEndGame;
         GameEvents.OnVictory += HandleVictory;
+
+        containers[ContainerType.Pause].ButtonNode.Pressed += HandlePausePressed;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (!canPause)
+        {
+            return;
+        }
+
+        if (!Input.IsActionJustPressed(GameConstants.INPUT_PAUSE))
+        {
+            return;
+        }
+
+        GetTree().Paused = !GetTree().Paused;
+        containers[ContainerType.Stats].Visible = !GetTree().Paused;
+        containers[ContainerType.Pause].Visible = GetTree().Paused;
     }
 
     private void HandleVictory()
     {
+        canPause = false;
+
         containers[ContainerType.Stats].Visible = false;
         containers[ContainerType.Victory].Visible = true;
 
@@ -32,15 +55,28 @@ public partial class UIController : Control
 
     private void HandleEndGame()
     {
+        canPause = false;
+
         containers[ContainerType.Stats].Visible = false;
         containers[ContainerType.Defeat].Visible = true;
     }
 
     private void HandleStartPressed()
     {
+        canPause = true;
+
         GetTree().Paused = false;
+
         containers[ContainerType.Start].Visible = false;
         containers[ContainerType.Stats].Visible = true;
         GameEvents.RaiseStartGame();
+    }
+
+    private void HandlePausePressed()
+    {
+        GetTree().Paused = false;
+
+        containers[ContainerType.Pause].Visible = false;
+        containers[ContainerType.Stats].Visible = true;
     }
 }
