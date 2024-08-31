93f1380632c5966edac34423f21b3eaac65e7bce
Ronan Burke <burkuscat@gmail.com>
2024-08-31 17:44:45 +0100

    8.10 Dynamically Applying a Shader

--- Dungeon RPG/Scripts/Characters/Character.cs
+++ Dungeon RPG/Scripts/Characters/Character.cs
@@ -13,6 +13,7 @@ public abstract partial class Character : CharacterBody3D
     [Export] public Area3D HurtboxNode { get; private set; }
     [Export] public Area3D HitboxNode { get; private set; }
     [Export] public CollisionShape3D HitboxShapeNode { get; private set; }
+    [Export] public Timer ShaderTimerNode { get; private set; }
 
 
     [ExportGroup("AI Nodes")]
@@ -23,10 +24,33 @@ public abstract partial class Character : CharacterBody3D
 
 
     public Vector2 direction = new();
+    private ShaderMaterial shader;
 
     public override void _Ready()
     {
+        if (SpriteNode.MaterialOverlay is ShaderMaterial shaderMaterial)
+        {
+            shader = shaderMaterial;
+        }
+
         HurtboxNode.AreaEntered += HandleHurtboxEntered;
+        SpriteNode.TextureChanged += HandleTextureChanged;
+        ShaderTimerNode.Timeout += HandleShaderTimeout;
+    }
+
+    private void HandleShaderTimeout()
+    {
+        // stop the shader flash effect
+        shader.SetShaderParameter(
+            "active", false
+        );
+    }
+
+    private void HandleTextureChanged()
+    {
+        shader.SetShaderParameter(
+            "tex", SpriteNode.Texture
+        );
     }
 
     private void HandleHurtboxEntered(Area3D area)
@@ -40,7 +64,12 @@ public abstract partial class Character : CharacterBody3D
         float damage = hitbox.GetDamage();
         health.StatValue -= damage;
 
-        GD.Print(health.StatValue);
+        shader.SetShaderParameter(
+            "active", true
+        );
+
+        // start the timer so we can deactivate flash after a moment
+        ShaderTimerNode.Start();
     }
 
     public StatResource GetStatResource(Stat stat)
